a79783e7cbbb3685c4d5cded0dca6e495ba0a0ef
RaquelFuentes <74673339+RaquelFuentes@users.noreply.github.com>
2020-12-09 18:55:35 -0300

    htdh

--- API_RENFERMEDADES/Controllers/Registro_EnfermedadController.cs
+++ API_RENFERMEDADES/Controllers/Registro_EnfermedadController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API_RENFERMEDADES.Modelos;
+using API_RENFERMEDADES.Conection;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
