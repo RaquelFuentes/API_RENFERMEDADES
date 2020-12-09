d14446dd6b6cf03e023b6fdc0e6664ea6c7dd29c
RaquelFuentes <74673339+RaquelFuentes@users.noreply.github.com>
2020-12-09 18:41:48 -0300

    as

---
+++ API_RENFERMEDADES/Controllers/Registro_EnfermedadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_RENFERMEDADES.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_RENFERMEDADES.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantaFamiliaController : ControllerBase
+    {
+        // GET: api/<PlantaFamiliaController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var lista = Registro_Enfermedad.ObtenerRegistro();
+            return new JsonResult(lista);
+
+        }
+    }
+}
